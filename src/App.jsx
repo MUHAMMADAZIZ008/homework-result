2c0afbd41d23c04a8df5150cd5aec79999f11991
MUHAMMADAZIZ <muhammadazizgulomov9@gmail.com>
2025-02-20 16:51:03 +0500

    added

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,35 @@
-import { useContext } from "react";
-import Form from "./components/form/form";
-import { TodoContext } from "./context/todo-context/todo-context";
-import { Route, Routes } from "react-router-dom";
-import MainLayout from "./layout/main-layout";
-import TaskList from "./pages/task-list/task-list";
-import CreateTodo from "./pages/create-todo/create-todo";
-import UpdateTodo from "./pages/update-todo/update-todo";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  const { data, dispatch } = useContext(TodoContext);
+  const [count, setCount] = useState(0)
 
   return (
-    <Routes>
-      <Route path="/" element={<MainLayout />}>
-        <Route index element={<TaskList />} />
-        <Route path="/create-todo" element={<CreateTodo />} />
-        <Route path="/update-todo/:id" element={<UpdateTodo />} />
-      </Route>
-    </Routes>
-  );
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
 }
 
-export default App;
+export default App
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
-import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
-import TodoContextProvider from "./context/todo-context/todo-context.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
-createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <TodoContextProvider>
-      <App />
-    </TodoContextProvider>
-  </BrowserRouter>
-);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
